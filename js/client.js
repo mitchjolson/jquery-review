82769c067c2dbb4485310e1a3ed1f312d8c9f863
Mitch Olson <mitchjolson@gmail.com>
2019-06-20 11:48:54 -0500

    added to notes

--- js/client.js
+++ js/client.js
@@ -10,7 +10,7 @@ $(document).ready(onReady);
 
 // appends the student as a list item to the #students element 
 function renderStudent() {
-    $(`#students`).append(`<li>${student}</li>`)
+    $(`#students`).append(`<li class="student">${student}</li>`)
 }
 
 // Loops through the entered list, calls the renderStudent function for each item in the list
@@ -20,7 +20,19 @@ function renderStudentList(studentList){
     }
 }
 
+// This function will be called when we click on a student
+// It will show an alert stating that the student is awesome
+function showAwesomeAlert(event){
+    console.log('Event:',event);
+    console.log(this);
+    alert('Check the console...')
+}
+
 // When DOM is ready, calls the renderStudentList function
+// Need to select something already on the DOM
+// Can filter the event to something new (dynamically generated)
+// Do this with the 2nd arg to 'on' function ( '.student' )
 function onReady(){
     renderStudentList(playfair);
+    $( '#students' ).on( 'click', '.student', showAwesomeAlert );
 }
